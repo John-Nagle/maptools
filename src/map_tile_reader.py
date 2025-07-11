127d8b3d4c5c8fd120c517d3ba5100eeee7683c8
John Nagle <nagle@sitetruth.com>
2025-07-10 22:13:21 -0700

    Working on map tile reader boundaries

--- src/map_tile_reader.py
+++ src/map_tile_reader.py
@@ -5,6 +5,7 @@
 #
 from urllib.request import urlopen
 import json
+from map_tile_defs import RegionBox
 
 #   A map tile URL looks like this: https://secondlife-maps-cdn.akamaized.net/map-4-1000-1000-objects.jpg
 MAP_FILENAME = "map-%i-%i-%i-objects.jpg"
@@ -39,7 +40,7 @@ Scan a rectangular area of the map and output. Save images.
 We only download tiles bigger than 256x256. because the
 size 1 tiles are available by UUID already.
 ''' 
-def download_map_rectangle(ll, ur, directory) :
+def download_map_rectangle_old(ll, ur, directory) :
     items = []
     for x in range(ll[0], ur[0]+1) : 
         for y in range(ll[1], ur[1]+1) :
@@ -58,9 +59,32 @@ def download_map_rectangle(ll, ur, directory) :
                     print("Failed for [%i, %i]: %s" % (x, y, err))
     print("Done.")
     return items
+    
+def download_map_rectangle(fullbox, directory) :
+    for lod in range (2,8) :
+        scaling = int(pow(2,lod-1))
+        # We want all boxes at this LOD which overlap fullbox
+        boxsize = scaling*256
+        print("LOD: %i, scaling %i, boxsize: %i" % (lod, scaling, boxsize))
+        #   Round down to get starting position that may be partly off edge of fullbox
+        xstart = int(int(fullbox.pos[0]) / boxsize) * boxsize
+        ystart = int(int(fullbox.pos[1]) / boxsize) * boxsize
+        xend = int(fullbox.pos[0] + fullbox.size[0])
+        yend = int(fullbox.pos[1] + fullbox.size[1])
+        print(xstart, xend, boxsize) 
+        for x in range (xstart, xend, boxsize) :
+            for y in range (ystart, yend, boxsize) :
+                thisbox = RegionBox([x, y], [boxsize, boxsize])
+                print(thisbox.pos)
+        
+    
 
 
 if __name__ == "__main__" :
-    ####test1()
     outdir = "tileimages"
-    download_map_rectangle([1130, 1046], [1139, 1054], outdir) # Blake Sea
+    boxll = RegionBox([1130*256, 1046*256], [256, 256])
+    boxur = RegionBox([1139*256, 1054*256], [256, 256])
+    # encloses all
+    fullbox = boxll.union(boxur)   
+    download_map_rectangle(fullbox, outdir)
+    ###### download_map_rectangle_old([1130, 1046], [1139, 1054], outdir) # Blake Sea
