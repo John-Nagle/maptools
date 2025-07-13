08c46ebf0991a5172729f36f4f1f82a4d79defc5
John Nagle <nagle@sitetruth.com>
2025-07-13 11:24:15 -0700

    Map tile calc begins

--- src/map_tile_calc.py
+++ src/map_tile_calc.py
@@ -1,5 +1,6 @@
 #   Map tile definitions
 #
+import math
 """
 Useful calculator for map tile math
 """
@@ -7,14 +8,16 @@ def calculator() :
     print("Map tile math calculator for Second Life map tiles.")
     while True:
         try:
-            print("Enter x,y")
-            line = input()
-            fields = line.split(",")
+            print("Enter x,y: ", end = "")
+            coordsline = input()
+            fields = coordsline.split(",")
             if (len(fields) == 2) :
                 coords = [int(fields[0]), int(fields[1])]
-                tilecalc(coords)
+                print("Enter size: ", end = "")
+                size = int(input())
+                tilecalc(coords, size)
             else :
-                print("Invalid input: %s" % (line))
+                print("Invalid input: %s" % (coordsline))
         except (KeyError, ValueError) as e :
             print("Error: %s", e)
         except EOFError:
@@ -23,16 +26,32 @@ def calculator() :
 """
 Basic tile math
 """
-def tilecalc(coords) :
+def tilecalc(coords, size) :
     x = coords[0]
     y = coords[1]
     if (x % 256 == 0) and (y % 256 == 0) :
         xtile = x / 256
         ytile = y / 256
         print("Tile X, Y: (%i,%i)" % (xtile, ytile))
+        if (size % 256 == 0) :
+            tilesize = size / 256
+            lod = lodcalc(tilesize)
+        else :
+            print("Size not a multiple of 256")
     else :
-        print ("Not a multiple of 256")
+        print ("x or y not a multiple of 256")
         
+"""
+Calculate LOD from size
+"""
+def lodcalc(size) :
+    lod = math.floor(math.log2(size))
+    print("LOD: %i" % lod)
+    if (pow(2,lod) != size) :
+        print("Size %i is not a power of 2" % (size))
+    lod
+    
+    
 if __name__ == "__main__" :
     calculator()
 
