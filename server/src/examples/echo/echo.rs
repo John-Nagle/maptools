b9df0b2ca5d88f4db30af31305f4a4cf85492214
John-Nagle <nagle@sitetruth.com>
2025-08-07 12:07:16 -0700

    Echo not working at all. outer_cgi a dead end

--- server/src/examples/echo/echo.rs
+++ server/src/examples/echo/echo.rs
@@ -2,6 +2,8 @@ use std::collections::HashMap;
 use outer_cgi::IO;
 
 fn handler(io: &mut dyn IO, env: HashMap<String, String>) -> anyhow::Result<i32> {
+    let mut all_data = Vec::new();
+    let sink = io.read_to_end(&mut all_data)?;
     io.write_all(format!(r#"Content-type: text/plain; charset=utf-8
 
 Hello World! Your request method was "{}"!
