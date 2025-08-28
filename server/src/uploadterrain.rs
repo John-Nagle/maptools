9de678347228c6ac397de23ad4c09222b9b96f0c
John-Nagle <nagle@sitetruth.com>
2025-08-27 21:13:00 -0700

    Decoding scale properly

--- server/src/uploadterrain.rs
+++ server/src/uploadterrain.rs
@@ -27,6 +27,8 @@ use serde::{Deserialize};
 const UPLOAD_CREDS_FILE: &str = "upload_credentials.txt";
 /// Database name for terrain info
 const DB_NAME: &str = "terrain";
+/// Default region size, used on grids that don't do varregions.
+const DEFAULT_REGION_SIZE: u32 = 256;
 
 /// Debug logging
 fn logger() {
@@ -48,21 +50,56 @@ pub struct UploadedRegionInfo {
     /// Grid name
     grid: String,
     /// Position of region in world, meters.
-    region_coords: [u32;2],
+    pub region_coords: [u32;2],
     /// Region size. 256 x 256 if ommitted.
     size: Option<[u32;2]>,
     /// Region name
     name: String,
     /// Height data, a long set of hex data.  
     elevs: String,
+    /// Scale factor for elevs
+    scale: f32,
+    /// Offset factor for elevs
+    /// actual = input*scale + offset
+    offset: f32,
     //  Water level
-    water_lev: f32,
+    pub water_lev: f32,
 }
 
 impl UploadedRegionInfo {
     /// Parse from string
-    fn parse(s: &str) -> Result<Self, Error> {
+    pub fn parse(s: &str) -> Result<Self, Error> {
         Ok(serde_json::from_str(s)?)
+    }
+    
+    /// Get size, applying default region size for non-varregions
+    pub fn get_size(&self) -> [u32;2] {
+        if let Some(size) = self.size {
+            size
+        } else {
+            [DEFAULT_REGION_SIZE, DEFAULT_REGION_SIZE]
+        }
+    }
+    
+    /// Get grid in canonial lowercase format
+    pub fn get_grid(&self) -> String {
+        self.grid.to_lowercase()
+    }
+    
+    /// Get region name in canonical lowercase format
+    pub fn get_name(&self) -> String {
+        self.name.to_lowercase()
+    }
+    
+    /// Get elevations as numbers before offsetting.
+    /// Input is a hex string representing one elev per byte.
+    pub fn get_unscaled_elevs(&self) -> Result<Vec<u8>, Error> {
+        Ok(hex::decode(&self.elevs)?)
+    }
+    
+    /// Scale the elevations
+    pub fn get_scaled_elevs(&self) -> Result<Vec<f32>, Error> {
+        Ok(self.get_unscaled_elevs()?.iter().map(|&v| ((v as f32) / 256.0) * self.scale + self.offset).collect())
     }
 }
 ///  Our handler
@@ -100,7 +137,7 @@ impl Handler for TerrainUploadHandler {
         //  Return something useful.
         let b = format!("Env: {:?}\nParams: {:?}\n", env, request.params).into_bytes();
         Response::write_response(out, request, http_response.as_slice(), &b)?;
-        Ok(())
+        Ok(())	
     }
 }
 
@@ -146,8 +183,9 @@ pub fn main() {
 
 #[test]
 fn parse_terrain() {
-    const TEST_JSON: &str = "{\"grid\":\"agni\",\"name\":\"Vallone\",\"scale\":1.092822,\"offset\":33.500740,\"water_lev\":20.000000,\"region_coords\":[1807,1199],\"elevs\":\"E7CAACA3A5A8ACAEB0B2B5B9BDC0C4C5C5C3C0BDB9B6B3B2B2B3B4B7BBBFC3C7CBCED1D3D\"}";
+    const TEST_JSON: &str = "{\"grid\":\"agni\",\"name\":\"Vallone\",\"scale\":1.092822,\"offset\":33.500740,\"water_lev\":20.000000,\"region_coords\":[1807,1199],\"elevs\":\"E7CAACA3A5A8ACAEB0B2B5B9BDC0C4C5C5C3C0BDB9B6B3B2B2B3B4B7BBBFC3C7CBCED1D3\"}";
     println!("TEST_JSON: {}", TEST_JSON);
     let parsed = UploadedRegionInfo::parse(TEST_JSON).expect("JSON misparsed");
     println!("Parsed JSON: {:?}", parsed);
+    println!("Elevs: {:?}", parsed.get_scaled_elevs());
 }
