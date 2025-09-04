613ccea7a7e171207205b2f2244354ae51d9200b
John-Nagle <nagle@sitetruth.com>
2025-09-03 23:45:05 -0700

    Sketching out transitive closure

--- server/src/vizgroup.rs
+++ server/src/vizgroup.rs
@@ -36,6 +36,35 @@ pub struct RegionData {
     name: String,
 }
 
+/// A rectangle of interest which might touch a object in an incoming column.
+pub struct LiveBlock {
+}
+
+/// An ordered sequence of LiveBlock items hwich might touch an object in an incoming column.
+/// When a new column comes in, any LiveBlock which doesn't reach that far is purged.
+//  Needs an ordered representation.
+struct LiveBlocks {
+    // ***MORE***
+}
+
+impl LiveBlocks {
+    /// Test for overlap between a column and the live blocks.
+    //  *** What should this return? ***
+    fn test_overlap(&self, column: &[RegionData]) -> usize {
+        todo!();
+    } 
+    /// Purge all blocks whose x edge is below the limit.
+    /// This is all of them on SL, but larger regions on OS might be kept.
+    fn purge_below_limit(lim: u32) -> Vec<LiveBlock> {
+        todo!();
+    }
+    
+    /// Add all the regions in a column to the live blocks.
+    fn add_column(&mut self, column: &[RegionData]) {
+        todo!();
+    }
+}
+
 /// A set of regions which all have the same viz group
 pub struct VizGroup {
     /// All in this viz group are in this string.
@@ -49,7 +78,7 @@ impl VizGroup {
 
     /// Merge two VizGroups, consuming them.
     pub fn merge(mut a: VizGroup, b: VizGroup) -> VizGroup {
-  	      assert_eq!(a.grid, b.grid);
+  	    assert_eq!(a.grid, b.grid);
         a.regions.extend(b.regions);
         Self {
             grid: a.grid,
