02f953fcd246eb7bb8ad1e116327e9a944a71564
John-Nagle <nagle@sitetruth.com>
2025-09-13 13:59:37 -0700

    Reorganize directories

---
+++ rust/src/common/lib.rs
@@ -0,0 +1,7 @@
+mod credentials;
+//////mod fcgisocketsetup;
+//////mod minifcgi;
+
+pub use credentials::Credentials;
+//////pub use fcgisocketsetup::init_fcgi;
+//////pub use minifcgi::{Handler, Request, Response, run};
--- server/src/lib.rs
+++
@@ -1,7 +0,0 @@
-mod credentials;
-mod fcgisocketsetup;
-mod minifcgi;
-
-pub use credentials::Credentials;
-pub use fcgisocketsetup::init_fcgi;
-pub use minifcgi::{Handler, Request, Response, run};
