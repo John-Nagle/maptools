eb4f7d7034b537aa7c673b67d70f8b14fd88a080
John-Nagle <nagle@sitetruth.com>
2026-02-07 20:53:37 -0800

    More of initial_impostors table

--- rust/src/generator/initialimpostors.rs
+++ rust/src/generator/initialimpostors.rs
@@ -14,7 +14,8 @@
 //!     February, 2026.
 //
 use anyhow::{Error, anyhow};
-use mysql::{PooledConn};
+use mysql::{PooledConn, params};
+use mysql::prelude::Queryable;
 use uuid::{Uuid};
 use crate::{RegionData};
 use common::{RegionImpostorData, RegionImpostorFaceData, HeightField};
@@ -37,8 +38,49 @@ impl InitialImpostors {
     }
     
     /// Add one impostor (sculpt or mesh) to the table. UUIDs may be null.
+    /// This is a pure insert into a table that starts empty. Duplicates should not happen.
     pub fn add_impostor(&mut self, region_impostor_data: RegionImpostorData) -> Result<(), Error> {
-        todo!();
+        log::debug!("Inserting {:?} into initial_impostors.", region_impostor_data.name);
+        //  We have all the info now. Update the region_impostor table.
+        //  Insert tile, or update hash and uuid if exists. 
+        const SQL_IMPOSTOR: &str = r"INSERT INTO initial_impostors
+                (grid, name, region_loc_x, region_loc_y, region_size_x, region_size_y, viz_group,
+                scale_x, scale_y, scale_z, 
+                elevation_offset, impostor_lod, viz_group, 
+                mesh_uuid, sculpt_uuid,
+                mesh_hash, sculpt_hasn
+                water_height, creation_time, faces_json) 
+            VALUES 
+                (:grid, :name, :region_loc_x, :region_loc_y, :region_size_x, :region_size_y, :viz_group,
+                :scale_x, :scale_y, :scale_z,
+                :elevation_offset, :impostor_lod, :viz_group, 
+                :mesh_uuid, :sculpt_uuid, 
+                :mesh_hash, :sculpt_hash,
+                :water_height, NOW(), :faces_json)";
+               
+        let insert_params = params! {
+                "grid" => region_impostor_data.grid.to_lowercase().clone(),
+                "name" => region_impostor_data.name,
+                "mesh_uuid" => region_impostor_data.mesh_uuid,
+                "sculpt_uuid" => region_impostor_data.sculpt_uuid,
+                "mesh_hash" => region_impostor_data.mesh_hash,
+                "sculpt_hash" => region_impostor_data.sculpt_hash,
+                "region_loc_x" => region_impostor_data.region_loc[0],
+                "region_loc_y" => region_impostor_data.region_loc[1],
+                "region_size_x" => region_impostor_data.region_size[0],
+                "region_size_y" => region_impostor_data.region_size[1],
+                "scale_x" => region_impostor_data.scale[0], // ***CONVERT TO INT***
+                "scale_y" => region_impostor_data.scale[1], // ***CONVERT TO INT***
+                "scale_z" => region_impostor_data.scale[2],
+                "impostor_lod" => region_impostor_data.impostor_lod,
+                "viz_group" => region_impostor_data.viz_group,
+                "elevation_offset" => region_impostor_data.elevation_offset,
+                "water_height" => region_impostor_data.water_height,
+                "faces_json" => serde_json::to_string(&region_impostor_data.faces)?,
+            };
+        //  Finally insert into the impostor table
+        log::debug!("Inserting impostor into initial_impostors, params: {:?}", insert_params);
+        Ok(self.conn.exec_drop(SQL_IMPOSTOR, insert_params)?)
     }
 }
 
