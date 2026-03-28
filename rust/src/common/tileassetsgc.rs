47fd872aab0e4aacdc6573275eb0d26a68df3c0b
John-Nagle <nagle@sitetruth.com>
2026-03-28 00:24:12 -0700

    Rewrite bulk insert, wrong col count

--- rust/src/common/tileassetsgc.rs
+++ rust/src/common/tileassetsgc.rs
@@ -132,6 +132,45 @@ impl TileGc {
         Ok(result?.into_iter().flatten().collect())
     }
     
+    /// Build temporary table rows
+    fn build_temporary_table_chunks(&self, tx: &mut Transaction) -> Result<(), Error> {
+       const INSERT_INUSE_UUID_ROWS: &str = r"INSERT INTO uuids_in_use
+            (region_loc_x, region_loc_y, region_size_x, region_size_y, asset_type, asset_uuid, asset_hash)
+            VALUES ";
+        const NAMED_PARAMETERS: [&str;7] = ["region_loc_x", "region_loc_y", "region_size_x", "region_size_y", "asset_type", "asset_uuid", "asset_hash"];
+        let named_parameters: Vec<_> = NAMED_PARAMETERS.iter().map(|p| p.to_string().into_bytes()).collect();
+        const CHUNK_SIZE: usize = 100;
+        let active_uuids = self.get_uuids_in_use(tx)?;
+        log::info!("{} active UUIDs: {:?}", active_uuids.len(), &active_uuids[0..3.min(active_uuids.len())]);
+        for active_uuid_chunk in active_uuids.chunks(CHUNK_SIZE) {
+            log::debug!("Chunk with {} items", active_uuid_chunk.len());
+            let mut value_strings = Vec::new();
+            for p in active_uuid_chunk {
+                let row_params = params!{
+                    "region_loc_x" => p.region_loc[0],
+                    "region_loc_y" => p.region_loc[1],
+                    "region_size_x" => p.region_size[0],
+                    "region_size_y" => p.region_size[1],
+                    "asset_type" => p.asset_type.to_str().to_string(),
+                    "asset_uuid" => p.asset_uuid.to_string(),
+                    "asset_hash" => p.asset_hash.clone(),                    
+                };
+                //  Make one row.
+                let row_string: String = row_params.into_values(Some(&named_parameters))?.into_iter().map(|v: mysql::Value| v.as_sql(false)).collect::<Vec<_>>().join(",");
+                //////log::debug!("Row string: {}", row_string);
+                value_strings.push(format!("({})", row_string));
+            }
+            //  All escaping was done above.
+            //  Make final VALUES clause
+            let sql = format!("{} ({})", INSERT_INUSE_UUID_ROWS, value_strings.join(",\n"));
+            log::debug!("Query: {}", sql);    // ***TEMP***
+            //////todo!();
+            
+            tx.query_drop(sql)?;           
+        }
+        Ok(())   
+    }
+    
     /// Build the temporary table of UUIDs in use.
     fn build_temporary_table(&self, tx: &mut Transaction) -> Result<(), Error> {
         const CREATE_INUSE_UUIDS_SQL: &str = r"CREATE TEMPORARY TABLE uuids_in_use (
@@ -147,15 +186,19 @@ impl TileGc {
             VALUES
             (:region_loc_x, :region_loc_y, :region_size_x, :region_size_y, :asset_type, :asset_uuid, :asset_hash)";
         //  Get all the active UUIDs. In memory all at once, but under 1MB
-        let active_uuids = self.get_uuids_in_use(tx)?;
-        log::info!("{} active UUIDs: {:?}", active_uuids.len(), &active_uuids[0..5.min(active_uuids.len())]);
+        //////let active_uuids = self.get_uuids_in_use(tx)?;
+        //////log::info!("{} active UUIDs: {:?}", active_uuids.len(), &active_uuids[0..5.min(active_uuids.len())]);
          //  Create the temporary table
         tx.query_drop(CREATE_INUSE_UUIDS_SQL)?;
         log::info!("Temporary table created.");
+        self.build_temporary_table_chunks(tx)?;
+/*
         //  Put all the records in the temporary table.
         tx.exec_batch(
             INSERT_INUSE_UUIDS_SQL,
-            active_uuids.iter().map(|p| params! {
+            active_uuids.iter().map(|p| {
+                log::debug!("Inserting {:?}", p.region_loc);    // ***TEMP***
+                params! {
                 "region_loc_x" => p.region_loc[0],
                 "region_loc_y" => p.region_loc[1],
                 "region_size_x" => p.region_size[0],
@@ -163,8 +206,9 @@ impl TileGc {
                 "asset_uuid" => p.asset_uuid.to_string(),
                 "asset_hash" => p.asset_hash.clone(),
                 "asset_type" => p.asset_type.to_str().to_string(),
-            })
+            }})
         )?;
+*/
         log::info!("Temporary table filled.");
         Ok(())
     }
