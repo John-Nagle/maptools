88892f86da4536fba20b4484b8b2ea048b569184
John-Nagle <nagle@sitetruth.com>
2026-04-04 15:04:06 -0700

    Getting floats OK

--- rust/src/ingest/fetchbonniebots.rs
+++ rust/src/ingest/fetchbonniebots.rs
@@ -23,13 +23,20 @@ const TERRAIN_DATA_SIZE: usize = 256*256;
 const USER_AGENT: &str = "animats.info impostor asset system";
 /// Fetch elevation data from Bonniebots. 256x256.
 pub fn fetch_elevs(agent: &mut Agent, region_num_x: u32, region_num_y: u32) -> Result<Option<[f32;TERRAIN_DATA_SIZE]>, Error> {
-    /// Build URL
+    // Build URL
     let url = format!("https://www.bonniebots.com/static-api/terrain/{}-{}.bin", region_num_x, region_num_y);        
     match ureq::get(&url).call() {
         Ok(mut response) => {
             let content = response.body_mut().read_to_vec()?;
             log::debug!("Length of content: {}", content.len());
-            Ok(None)    // ***TEMP***
+            const FLOAT_SIZE: usize = 4;    // this is a constant somewhere
+            if content.len() != TERRAIN_DATA_SIZE*FLOAT_SIZE {
+                Err(anyhow!("Elevation data has wrong size: {}. Expected {}. URL: {}", content.len(), TERRAIN_DATA_SIZE*FLOAT_SIZE, url))
+            } else {
+                let elevs: Vec<_> = content.chunks(FLOAT_SIZE).map(|c: &[u8]| f32::from_le_bytes(c.try_into().unwrap())).collect();
+                log::debug!("Elevs: {:?}", &elevs[0..4]);  // ***TEMP***
+                Ok(None)    // ***TEMP***
+            }
         }
         Err(ureq::Error::StatusCode(code)) => {
            // the server returned an unexpected status
@@ -47,7 +54,7 @@ fn test_fetchelevs() {
     use common::test_logger;
     test_logger();
     //  HTTP Agent
-    let mut config = Agent::config_builder()       
+    let config = Agent::config_builder()       
         .build();
     let mut agent: Agent = config.into();
     let elevs = fetch_elevs(&mut agent, 1000, 1000).expect("Fetch from BonnieBots failed.");
