6e0e9335384d938068d7fcdae6f1e0c7f2c95253
John-Nagle <nagle@sitetruth.com>
2026-04-04 13:52:12 -0700

    Beginnings of BonnieBots reader

---
+++ rust/src/ingest/fetchbonniebots.rs
@@ -0,0 +1,10 @@
+//! fetchbonniebots.rs -- obtain Second Life / Open Simulator terrain data from bonniebots.
+//!
+//! Part of the Animats impostor system
+//!
+//! This runs as a command line program, or perhaps a cron job.
+//!
+//!     License: LGPL.
+//!     Animats
+//!     April, 2026.
+//!
