59dab93643f36b008d3d42c5ab1bc9e190e425ef
John-Nagle <nagle@sitetruth.com>
2026-04-07 22:11:50 -0700

    Beginnings of rect filter

--- rust/src/common/rect.rs
+++ rust/src/common/rect.rs
@@ -6,22 +6,35 @@
 //!     Animats
 //!     April, 2026.
 //!
+use std::fmt::Debug;
+use std::str::FromStr;
+use regex::Regex;
+use anyhow::{Error, anyhow};
 
 /// Rect - 2D rectangle.
 #[derive(Debug, Clone, PartialEq)]
-pub struct Rect<T: PartialEq+PartialOrd> {
+pub struct Rect<T: PartialEq+PartialOrd+Debug+std::str::FromStr> {
     //  Lower left
     ll: [T;2],
     //  Upper right
     ur: [T;2],
 }
 
-impl<T: PartialEq+PartialOrd> Rect<T> {
+impl<T: PartialEq+PartialOrd+Debug+std::str::FromStr> Rect<T> {
     /// Usual new
     pub fn new(ll: [T;2], ur: [T;2]) -> Self {
         assert!(ur[0] >= ll[0]);
         assert!(ur[1] >= ll[1]);
         Self { ll, ur }
+    }
+    
+    /// New with error handling
+    pub fn try_new(ll: [T;2], ur: [T;2]) -> Result<Self, Error> {
+        if ur[0] >= ll[0] || ur[1] >= ll[1] {
+            Ok(Self { ll, ur })
+        } else {
+            Err(anyhow!("Corners of rectangle ({:?}- {:?}) are not ordered lower left, upper right", ll, ur))
+        }
     }
     
     /// True if overlaps. Not just touches.
@@ -40,10 +53,23 @@ impl<T: PartialEq+PartialOrd> Rect<T> {
     pub fn is_empty(&self) -> bool {
         self.ll[0] == self.ur[0] || self.ll[1] == self.ur[1]
     }
+    
+    /// Parse (x0,y0)-(x1,y1) forms.
+    pub fn parse(s: &str) -> Result<Self, Error> where <T as FromStr>::Err: std::fmt::Debug {
+        //////let re = Regex::new(r"(?m)^([^:]+):([0-9]+):(.+)$").unwrap();
+        let re = Regex::new(r"\(([0-9]+)\,([0-9]+)\)\-\(([0-9]+)\,([0-9]+)\)").unwrap();
+        let vals = re.captures(s).ok_or_else(|| anyhow!("Cannot parse rectangle bounds (n,n)-(n,n) from \"{}\"", s))?;
+        println!("Vals: {:?}", vals);
+        let llx: T = vals[1].parse().unwrap();
+        let lly: T = vals[2].parse().unwrap();
+        let urx: T = vals[3].parse().unwrap();
+        let ury: T = vals[4].parse().unwrap();
+        Self::try_new([llx, lly], [urx, ury])
+    }
 }
 
 // Implement Display for Rect
-impl<T: std::fmt::Display+std::cmp::PartialOrd> std::fmt::Display for Rect<T> {
+impl<T: std::fmt::Display+std::cmp::PartialOrd+Debug+std::str::FromStr> std::fmt::Display for Rect<T> {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         // Write the formatted string into the formatter
         write!(f, "({}, {})-({}, {})", self.ll[0], self.ll[1], self.ur[0], self.ur[1])
@@ -64,4 +90,8 @@ fn test_rect() {
     assert!(!r0.overlaps(&r1));
     assert!(r1.overlaps(&r2));
     println!("r2: {}", r2);
+    
+    let s = "(21,20)-(29,30)";
+    let parsed = Rect::<u32>:: parse(s).expect("Parse failed");
+    assert_eq!(parsed, r2);
 }
